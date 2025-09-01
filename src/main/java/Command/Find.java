25f5ab967251d4699143c2ccc3e86b83d217d50a
siangjun2 <siangjun265@gmail.com>
2025-09-01 16:37:31 +0800

    Find : Add JavaDoc

--- src/main/java/Command/Find.java
+++ src/main/java/Command/Find.java
@@ -9,13 +9,30 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
+/**
+ * Matches all {@link Task} in task list that have given string
+ * in description.
+ */
 public class Find implements Command {
+    /**
+     * Substring to be found in description
+     */
     private final String text;
 
+    /**
+     * Initialises command object with substring
+     * @param text Substring to be found
+     */
     public Find(String text) {
         this.text = text;
     }
 
+    /**
+     * Matches all tasks in storage task list that have a
+     * description containing given substring.
+     * @param storage   Storage under consideration
+     * @throws DukeException    Handles any error during execution
+     */
     @Override
     public void execute(Storage storage) throws DukeException {
         List<Task> tasks = storage.getTasks();
