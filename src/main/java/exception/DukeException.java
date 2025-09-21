42f94a39d021dc75decee33825b7aa2c1309b05c
siangjun2 <siangjun265@gmail.com>
2025-09-22 01:28:50 +0800

    Update DukeException.java

--- src/main/java/exception/DukeException.java
+++ src/main/java/exception/DukeException.java
@@ -1,7 +1,8 @@
 package exception;
 
 /**
- * Represents custom exception for DukeGpt. Can be extended for more specific functionalities
+ * Represents custom exception for DukeGpt.
+ * Can be extended for more specific functionalities
  */
 public class DukeException extends Exception {
     public DukeException(String message) {
